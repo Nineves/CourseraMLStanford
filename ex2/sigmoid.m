9f43eb3cbfb64cd8fc3cc5ac85b585b2548e16f7
Nineves <73009417+Nineves@users.noreply.github.com>
2021-08-30 17:31:23 +0800

    Add files via upload

---
+++ ex2/sigmoid.m
@@ -0,0 +1,26 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+s=size(z);
+if(s(2)>=1)
+    for i=1:s(1)
+        for j=1:s(2)
+            g(i,j)=1/(1+exp(-z(i,j)));
+        end
+    end
+else
+    g=1/(1+exp(-z));
+end
+
+
+
+% =============================================================
+
+end
